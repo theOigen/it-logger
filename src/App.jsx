5236bd57f4d471db9e40734b705b4d0dfbe5ca9c
theoigen <oigenZ@icloud.com>
2019-07-25 21:28:22 +0300

    added modal component to create new logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/Logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/Logs/AddLogModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -14,6 +16,8 @@ const App = () => {
     <>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </>
